345556ba7aecb351d69267690a44c91540b78c57
Maksim Golev <mgolev@htc-cs.ru>
2023-09-05 17:00:03 +0400

    Trying to implement first lab.

--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -1,5 +1,35 @@
 package module1.homework
 
 object LinearAlgebraOps{
-  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = ???
+  def sum(v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    if (v1.length != v2.length) {
+      throw new Exception("Operation is not supported");
+    } else {
+      var returnValue = Array[Int](v1.length);
+      for (i <- v1.indices) {
+        returnValue(i) = v1(i) + v2(i);
+      }
+      returnValue;
+    }
+  }
+
+  def scale(a: Int, v: Array[Int]): Array[Int] = {
+    var returnValue = Array[Int](v.length);
+    for (i <- v.indices) {
+      returnValue(i) = a * v(i);
+    }
+    returnValue;
+  }
+
+  def axpy(a: Int, v1: Array[Int], v2: Array[Int]): Array[Int] = {
+    if (v1.length != v2.length) {
+      throw new Exception("Operation is not supported");
+    } else {
+      var returnValue = Array[Int](v1.length);
+      for (i <- v1.indices) {
+        returnValue(i) = a * v1(i) + v2(i);
+      }
+      returnValue;
+    }
+  }
 }
