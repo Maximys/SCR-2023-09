3662ee3d9c12eb4aef218b723cbe8e343cfc375a
Maksim Golev <mgolev@htc-cs.ru>
2023-09-06 08:22:07 +0400

    Fix invalid array declaration.

--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -5,7 +5,7 @@ object LinearAlgebraOps{
     if (v1.length != v2.length) {
       throw new Exception("Operation is not supported");
     } else {
-      var returnValue = Array[Int](v1.length);
+      var returnValue = Array.ofDim[Int](v1.length);
       for (i <- 1 to v1.length) {
         returnValue(i) = v1(i) + v2(i);
       }
@@ -14,7 +14,7 @@ object LinearAlgebraOps{
   }
 
   def scale(a: Int, v: Array[Int]): Array[Int] = {
-    var returnValue = Array[Int](v.length);
+    var returnValue = Array.ofDim[Int](v.length);
     for (i <- 1 to v.length) {
       returnValue(i) = a * v(i);
     }
@@ -25,7 +25,7 @@ object LinearAlgebraOps{
     if (v1.length != v2.length) {
       throw new Exception("Operation is not supported");
     } else {
-      var returnValue = Array[Int](v1.length);
+      var returnValue = Array.ofDim[Int](v1.length);
       for (i <- 1 to v1.length) {
         returnValue(i) = a * v1(i) + v2(i);
       }
