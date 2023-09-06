125b2562c1a2a8e2169d961dc6b5f1d02b6e5373
Maksim Golev <mgolev@htc-cs.ru>
2023-09-06 08:28:04 +0400

    Fix invalid iteration over array.

--- src/main/scala/module1/homework/LinearAlgebraOps.scala
+++ src/main/scala/module1/homework/LinearAlgebraOps.scala
@@ -6,7 +6,7 @@ object LinearAlgebraOps{
       throw new Exception("Operation is not supported");
     } else {
       var returnValue = Array.ofDim[Int](v1.length);
-      for (i <- 1 to v1.length) {
+      for (i <- 0 until v1.length) {
         returnValue(i) = v1(i) + v2(i);
       }
       returnValue;
@@ -15,7 +15,7 @@ object LinearAlgebraOps{
 
   def scale(a: Int, v: Array[Int]): Array[Int] = {
     var returnValue = Array.ofDim[Int](v.length);
-    for (i <- 1 to v.length) {
+    for (i <- v.indices) {
       returnValue(i) = a * v(i);
     }
     returnValue;
@@ -26,7 +26,7 @@ object LinearAlgebraOps{
       throw new Exception("Operation is not supported");
     } else {
       var returnValue = Array.ofDim[Int](v1.length);
-      for (i <- 1 to v1.length) {
+      for (i <- 0 until v1.length) {
         returnValue(i) = a * v1(i) + v2(i);
       }
       returnValue;
