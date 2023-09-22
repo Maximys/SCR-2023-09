53cfc79a324406c951c94c33c0053cb75c87a8fb
Maksim Golev <mgolev@htc-cs.ru>
2023-09-22 10:26:05 +0400

    Implementation of probability calculation.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import module1.homework.collections.laboratoryWork3.bucket
 import module1.threads.{Thread1, ToyFuture, getRatesLocation1, getRatesLocation2, getRatesLocation3, getRatesLocation4, printRunningTime}
 import module1.{executor, hof, lazyOps, list, type_system}
 
@@ -8,38 +9,12 @@ object Main {
     println("Hello, World!" +
       s" thread - ${Thread.currentThread().getName}" )
 
-//    val t1 = new Thread{
-//      override def run(): Unit ={
-//        Thread.sleep(1000)
-//        println(s"Hello ${Thread.currentThread().getName}" )
-//      }
-//    }
-//    val t2 = new Thread{
-//      override def run(): Unit ={
-//        Thread.sleep(2000)
-//        println(s"Hello ${Thread.currentThread().getName}" )
-//      }
-//    }
-//    t2.start()
-//    t1.start()
+    val bucketsCount = 10000;
 
-    def rates = {
-       val t1 = ToyFuture(10)(executor.pool1)
-       val t2 = ToyFuture(20)(executor.pool1)
-
-       t1.onComplete{ i1 =>
-         t2.onComplete{i2 =>
-           println(i1 + i2)
-         }
-       }
-
-       val r: ToyFuture[Unit] = for{
-         i1 <- t1
-         i2 <- t2
-       } yield println(i1 + i2)
-    }
-
-    printRunningTime(rates)
-
+    val buckets: IndexedSeq[bucket] = (1 to bucketsCount).map(_ => new bucket());
+    val experimentalResult = buckets.map(b => b.isWhite);
+    val isWhiteCount = experimentalResult.count(r => r);
+    val probabilisticDistributionOfWhiteBall = isWhiteCount / bucketsCount.toFloat;
+    println(s"Рассчитанная вероятность получения белых шаров из ${bucketsCount} корзин равна ${probabilisticDistributionOfWhiteBall}");
   }
 }
