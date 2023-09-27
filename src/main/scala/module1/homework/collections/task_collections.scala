fe3d4f9d5fc1df5155711beebc9fee743a87b32f
Maksim Golev <mgolev@htc-cs.ru>
2023-09-27 08:36:57 +0400

    Implementation of "task_collections.numbersToNumericString" function.

--- src/main/scala/module1/homework/collections/task_collections.scala
+++ src/main/scala/module1/homework/collections/task_collections.scala
@@ -39,7 +39,19 @@ object task_collections {
    * HINT: Для всех возможных комбинаций чисел стоит использовать Map
    * **/
   def numbersToNumericString(text: String): String = {
-    ""
+    text.map {
+      case '0' => "zero"
+      case '1' => "one"
+      case '2' => "two"
+      case '3' => "three"
+      case '4' => "four"
+      case '5' => "five"
+      case '6' => "six"
+      case '7' => "seven"
+      case '8' => "eight"
+      case '9' => "nine"
+      case other => other.toString
+    }.mkString("")
   }
 
   /**
