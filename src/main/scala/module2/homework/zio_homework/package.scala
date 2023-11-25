c28c07bbdc1a45d9cfe1cabe74da412cf65b7598
Maksim Golev <mixim33@yandex.ru>
2023-11-25 22:05:59 +0400

    Implementation usages of ZIO.

--- src/main/scala/module2/homework/zio_homework/package.scala
+++ src/main/scala/module2/homework/zio_homework/package.scala
@@ -1,7 +1,8 @@
 package module2.homework
 
-import zio.{Has, Task, ULayer, ZIO, ZLayer}
-import zio.clock.{Clock, sleep}
+import module2.homework.zio_homework.utils.printEffectRunningTime
+import zio.{Has, Schedule, Task, ULayer, ZIO, ZLayer}
+import zio.clock.{Clock, currentTime, sleep}
 import zio.console._
 import zio.duration.durationInt
 import zio.macros.accessible
@@ -11,24 +12,62 @@ import java.io.IOException
 import java.util.concurrent.TimeUnit
 import scala.io.StdIn
 import scala.language.postfixOps
+import scala.util.Try
 
 package object zio_homework {
   /**
    * 1.
    * Используя сервисы Random и Console, напишите консольную ZIO программу которая будет предлагать пользователю угадать число от 1 до 3
-   * и печатать в когнсоль угадал или нет. Подумайте, на какие наиболее простые эффекты ее можно декомпозировать.
+   * и печатать в консоль угадал или нет. Подумайте, на какие наиболее простые эффекты ее можно декомпозировать.
    */
 
+  //сгенерировать случайное число
+  //позволить пользователю ввести число
+  //сравнить сгенерированное число и ввод пользователя
+  //вывести результат сравнения
 
+  val leftInclusiveRandomBorder = 1;
+  val rightExclusiveRandomBorder = 2;
 
-  lazy val guessProgram = ???
+  def requestNumberInTheRange(leftBorder: Int, rightBorder: Int) =
+    ZIO.fromTry(Try {
+      for {
+        _ <- putStrLn(s"Введите число от ${leftBorder} до ${rightBorder}")
+        rawInputValue <- getStrLn
+        intValue <- ZIO.effect(rawInputValue.toInt)
+        _ <- ZIO.effect(if ((intValue < leftBorder)
+          || (intValue >= rightBorder)) {
+          throw new Exception(s"Введенное число должно быть больше или равно ${leftBorder} и меньше ${rightBorder}");
+        })
+      } yield intValue
+    }).retry(Schedule.forever)
+
+  lazy val guessProgram = for {
+    hiddenNumber: Int <- nextIntBetween(leftInclusiveRandomBorder, rightExclusiveRandomBorder)
+    inputValue <- zio.Runtime.default.unsafeRun(
+      requestNumberInTheRange(leftInclusiveRandomBorder, rightExclusiveRandomBorder))
+    equals <- ZIO.succeed(hiddenNumber == inputValue)
+    _ <- if (equals) {
+      putStrLn("Поздравляем Вас! Загаданное число и число, введенное Вами, равны");
+    } else {
+      putStrLn("Сожалеем, но Вы не угадали число");
+    }
+  } yield equals
 
   /**
    * 2. реализовать функцию doWhile (общего назначения), которая будет выполнять эффект до тех пор, пока его значение в условии не даст true
    * 
    */
 
-  def doWhile = ???
+  def doWhile[R, E, A](effect: ZIO[R, E, A])(condition: A => Boolean): ZIO[R, E, A] =
+    for {
+      intermediateResult <- effect
+      returnValue <- if (condition(intermediateResult)) {
+        ZIO.succeed(intermediateResult)
+      } else {
+        doWhile(effect)(condition)
+      }
+    } yield returnValue
 
 
   /**
@@ -39,15 +78,22 @@ package object zio_homework {
 
 
   /**
-   * 3.1 Создайте эффект, который будет возвращать случайеым образом выбранное число от 0 до 10 спустя 1 секунду
+   * 3.1 Создайте эффект, который будет возвращать случайным образом выбранное число от 0 до 10 спустя 1 секунду
    * Используйте сервис zio Random
    */
-  lazy val eff = ???
+  val leftInclusiveRandomBorderForEff = 1;
+  val rightExclusiveRandomBorderForEff = 11;
+
+  lazy val eff =
+    for {
+      _ <- ZIO.sleep(1 second)
+      returnValue <- nextIntBetween(leftInclusiveRandomBorderForEff, rightExclusiveRandomBorderForEff)
+    } yield returnValue
 
   /**
-   * 3.2 Создайте коллукцию из 10 выше описанных эффектов (eff)
+   * 3.2 Создайте коллекцию из 10 выше описанных эффектов (eff)
    */
-  lazy val effects = ???
+  lazy val effects = List.range(0, 10).map(r => eff)
 
   
   /**
@@ -56,36 +102,85 @@ package object zio_homework {
    * можно использовать ф-цию printEffectRunningTime, которую мы разработали на занятиях
    */
 
-  lazy val app = ???
+  lazy val app = printEffectRunningTime(
+    for {
+      _ <- putStrLn("Начинаем расчет")
+      sum <- ZIO.reduceAll(ZIO.succeed(0), effects)((accum, currentValue) => accum + currentValue)
+      _ <- putStrLn(s"Вычисленная сумма равна ${sum}")
+    } yield ()
+  )
 
 
   /**
-   * 3.4 Усовершенствуйте программу 4.3 так, чтобы минимизировать время ее выполнения
+   * 3.4 Усовершенствуйте программу 3.3 так, чтобы минимизировать время ее выполнения
    */
 
-  lazy val appSpeedUp = ???
+  lazy val appSpeedUp = ZIO.reduceAllPar(ZIO.succeed(0), effects)((accum, currentValue) => accum + currentValue);
 
 
   /**
    * 4. Оформите ф-цию printEffectRunningTime разработанную на занятиях в отдельный сервис, так чтобы ее
    * молжно было использовать аналогично zio.console.putStrLn например
    */
+  object Stopwatch {
+    type StopwatchEnvironment = Has[Stopwatch.StopwatchService]
 
+    trait StopwatchService {
+      def start(): ZIO[Any, Throwable, Unit]
+
+      def stop(): ZIO[Any, IOException, Unit]
+    }
+
+    val live: ZLayer[Console with Clock, Nothing, StopwatchEnvironment] =
+      ZLayer.fromServices[Console.Service, Clock.Service, Stopwatch.StopwatchService] {
+        (console, clock) =>
+          new StopwatchService {
+            var startTime: Long = 0
+
+            override def start(): ZIO[Any, Throwable, Unit] = for {
+              currentTime <- clock.currentTime(TimeUnit.MILLISECONDS)
+              _ <- Task {
+                startTime = currentTime
+              }
+            } yield ()
+
+
+            override def stop(): ZIO[Any, IOException, Unit] = {
+              for {
+                currentTime <- clock.currentTime(TimeUnit.MILLISECONDS)
+                _ <- console.putStrLn(s"Эффект исполнялся ${currentTime - startTime} миллисекунд")
+              } yield ()
+
+            }
+          }
+      }
+
+    def start(): ZIO[StopwatchEnvironment, Throwable, Unit] = ZIO.accessM(_.get.start())
+
+    def stop(): ZIO[StopwatchEnvironment, Throwable, Unit] = ZIO.accessM(_.get.stop())
+  }
 
    /**
      * 5.
-     * Воспользуйтесь написанным сервисом, чтобы созадть эффект, который будет логировать время выполнения прогаммы из пункта 4.3
+     * Воспользуйтесь написанным сервисом, чтобы созадть эффект, который будет логировать время выполнения прогаммы из пункта 3.4
      *
      * 
      */
 
-  lazy val appWithTimeLogg = ???
+  lazy val appWithTimeLogg = for {
+    _ <- Stopwatch.start()
+    v <- appSpeedUp
+    _ <- Stopwatch.stop()
+  } yield v
 
   /**
     * 
     * Подготовьте его к запуску и затем запустите воспользовавшись ZioHomeWorkApp
     */
 
-  lazy val runApp = ???
+  lazy val runApp = for {
+    l <- appWithTimeLogg
+    _ <- putStrLn(l.toString)
+  } yield l
 
 }
